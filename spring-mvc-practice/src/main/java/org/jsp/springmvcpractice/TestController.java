1e8abd71aa6478efaaa67cf0cfe9c861fe2b9e50
sathishnyadav <90254530+sathishnyadav@users.noreply.github.com>
2024-06-09 12:06:00 +0530

    implementation of arithmetic operations by spring mvc

--- spring-mvc-practice/src/main/java/org/jsp/springmvcpractice/TestController.java
+++ spring-mvc-practice/src/main/java/org/jsp/springmvcpractice/TestController.java
@@ -1,7 +1,9 @@
 package org.jsp.springmvcpractice;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,6 +11,46 @@ public class TestController {
 	@RequestMapping(value = "/test")
 	@ResponseBody
 	public String test() {
-	 return "<h1>This handler method is created to test the Configuration</h1>";
+		return "<h1>This handler method is created to test the Configuration</h1>";
+	}
+
+	@RequestMapping(value = "/open-view")
+	public String openView(@RequestParam(name = "view") String view) {
+		return view;
+	}
+
+	@RequestMapping("/sum")
+	public String findSum(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+		String result = n1 + "+" + n2 + "=" + (n1 + n2);
+		model.addAttribute("res", result);
+		return "print";
+	}
+
+	@RequestMapping("/diff")
+	public String findDifference(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+		String result = n1 + "-" + n2 + "=" + (n1 - n2);
+		model.addAttribute("res", result);
+		return "print";
+	}
+
+	@RequestMapping("/prod")
+	public String findProduct(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+		String result = n1 + "*" + n2 + "=" + (n1 * n2);
+		model.addAttribute("res", result);
+		return "print";
+	}
+
+	@RequestMapping("/quotient")
+	public String findQuotient(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+		String result = n1 + "/" + n2 + "=" + (n1 / n2);
+		model.addAttribute("res", result);
+		return "print";
+	}
+
+	@RequestMapping("/rem")
+	public String findReminder(@RequestParam(name = "n1") int n1, @RequestParam(name = "n2") int n2, Model model) {
+		String result = n1 + "%" + n2 + "=" + (n1 % n2);
+		model.addAttribute("res", result);
+		return "print";
 	}
 }
---
+++ spring-mvc-practice/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,29 @@
+<html>
+<body>
+	<h2>
+		<a href="test">Click Here to Test Your Configuration</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=home">Click Here to view Home Page</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=findSum">Click here to find the sum</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=findDiff">Click here to find the
+			difference</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=findProduct">Click here to find the
+			product</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=findQuotient">Click here to find the
+			Quotient</a>
+	</h2>
+	<h2>
+		<a href="open-view?view=findReminder">Click here to find the
+			Reminder</a>
+	</h2>
+</body>
+</html>
