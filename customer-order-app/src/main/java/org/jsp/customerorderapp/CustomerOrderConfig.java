070eb690829cd7a5dc668af4a057b9ae6b260752
AlokRanjankumar <alokmvit2232@gmail.com>
2024-08-17 22:22:13 +0530

    first commit

--- customer-order-app/src/main/java/org/jsp/customerorderapp/CustomerOrderConfig.java
+++ customer-order-app/src/main/java/org/jsp/customerorderapp/CustomerOrderConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration1
 @ComponentScan({ "org.jsp.customerorderapp" })
 public class CustomerOrderConfig {
 	@Bean
